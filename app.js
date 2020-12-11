e258a904083454cbeeaeeb3b010b65125d7ccce8
CristianTriscu <triscu.cristian@gmail.com>
2020-12-11 14:01:49 +0200

    .

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { app, router } from "./init/serverinit.js"
-import { Proiect } from "./sequelize/sequelize.js"
+import { Proiect, Categorie } from "./sequelize/sequelize.js"
 
 router.route("/projects").get((req, res) => {
 
@@ -48,6 +48,28 @@ router.route("/projects/:id_proiect").delete((req, res) =>
     }
   }).then((result) => res.json(result))
 );
+
+
+
+
+router.route("/sequelize/projectsWithCategories").post((req, res) => {
+  Proiect.create({
+
+    descriere: req.body.descriere,
+    denumire: req.body.denumire
+  }).then(proiect => {
+
+    req.body.Categorie.forEach(categorie => {
+      Categorie.create({
+        id_categorie: categorie.body.id_categorie,
+        denumire_categ: categorie.denumire_categ,
+        descriere_categ: categorie.descriere_categ,
+        id_proiect: proiect.id_proiect
+      }).then(response => res.json(response)).catch(err=>console.log(err))
+    })
+
+  }).catch(err=>console.log(err))
+})
 
 
 var port = 8001;
--- sequelize/sequelize.js
+++ sequelize/sequelize.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize"
 
-export const sequelize = new Sequelize('bd_proiect_tw', 'root', '', {
+export const sequelize = new Sequelize('bd_licenta', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
 })
@@ -19,9 +19,25 @@ const Proiect = sequelize.define("project", {
 
 })
 
-export { Proiect }
+
+const Categorie = sequelize.define("category", {
+  id_categorie: {
+    type: Sequelize.INTEGER(11),
+    autoIncrement: true,
+    primaryKey: true
+  },
+  denumire_categ: Sequelize.STRING(300),
+  descriere_categ: Sequelize.STRING(300)
 
 
+})
+
+export { Proiect ,Categorie}
+
+
+
+Proiect.hasMany(Categorie,{foreignKey:"id_proiect",foreignKeyConstraint:true});
+Categorie.belongsTo(Proiect,{foreignKey:"id_proiect"});
 
 
 
@@ -35,4 +51,4 @@ sequelize.authenticate()
   .catch(err => console.error("Unable to connect to the database:" + err));
 
 
-sequelize.sync({ force: false, alter: false }).then(() => { console.log("Sync completed") }).catch(err => console.log("Error at creating: ") + err);
+sequelize.sync({ force: true, alter: true }).then(() => { console.log("Sync completed") }).catch(err => console.log("Error at creating: ") + err);
